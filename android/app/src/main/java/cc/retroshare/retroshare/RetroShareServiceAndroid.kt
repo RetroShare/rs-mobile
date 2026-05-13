1843a8d854f732b57041518af9cd4042da0bdf4d
defnax <9952056+defnax@users.noreply.github.com>
2026-05-13 13:56:40 +0200

    fix rsmobile

--- android/app/src/main/java/cc/retroshare/retroshare/RetroShareServiceAndroid.kt
+++ android/app/src/main/java/cc/retroshare/retroshare/RetroShareServiceAndroid.kt
@@ -56,7 +56,11 @@ class RetroShareServiceAndroid : Service() {
             .setSmallIcon(android.R.drawable.ic_menu_info_details)
             .build()
 
-        startForeground(1, notification)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(1, notification, android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+        } else {
+            startForeground(1, notification)
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
