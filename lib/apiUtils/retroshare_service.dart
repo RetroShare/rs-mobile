f9eecfa3bb6a69e9a2e1057dbb938cc196741369
defnax <9952056+defnax@users.noreply.github.com>
2026-06-30 22:47:52 +0200

    try for bdboot update

--- lib/apiUtils/retroshare_service.dart
+++ lib/apiUtils/retroshare_service.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 import 'package:retroshare_api_wrapper/retroshare.dart' as rs;
 
 const rsPlatform = MethodChannel(rs.RETROSHARE_CHANNEL_NAME);
@@ -75,5 +77,45 @@ class RsServiceControl {
     } catch (err) {
       throw Exception('The service could not be restarted');
     }
+  }
+}
+
+Future<void> copyBdbootToConfigDir() async {
+  try {
+    final supportDir = await getApplicationSupportDirectory();
+    final parentDir = supportDir.parent;
+
+    // We will copy to both the parent directory and the files directory config locations
+    final configPaths = [
+      Directory('${parentDir.path}/.retroshare'),
+      Directory('${supportDir.path}/.retroshare'),
+    ];
+
+    final data = await rootBundle.load('assets/bdboot.txt');
+    final bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    for (final rsConfigDir in configPaths) {
+      if (!await rsConfigDir.exists()) {
+        await rsConfigDir.create(recursive: true);
+      }
+
+      final bdbootFile = File('${rsConfigDir.path}/bdboot.txt');
+      await bdbootFile.writeAsBytes(bytes);
+      print('Successfully copied bdboot.txt to config root: ${rsConfigDir.path}');
+
+      // List all entities in the `.retroshare` directory to copy into individual location folders
+      await for (final entity in rsConfigDir.list(recursive: false)) {
+        if (entity is Directory) {
+          final dirName = entity.path.split(Platform.pathSeparator).lastWhere((element) => element.isNotEmpty);
+          if (dirName.startsWith('LOC06_')) {
+            final targetBdboot = File('${entity.path}/bdboot.txt');
+            await targetBdboot.writeAsBytes(bytes);
+            print('Successfully copied bdboot.txt to location directory: ${entity.path}');
+          }
+        }
+      }
+    }
+  } catch (e) {
+    print('Error copying bdboot.txt to config directories: $e');
   }
 }
